dd7e0fd90777fc94871d7f2e12a5d65863b2af06
Wicorel <saettler@hotmail.com>
2017-10-06 20:08:21 -0700

    formatting

--- MDK Orbital Descent/MDK Orbital Descent/ProcessArguments.cs
+++ MDK Orbital Descent/MDK Orbital Descent/ProcessArguments.cs
@@ -20,89 +20,90 @@ namespace IngameScript
     {
 
 
+        float fMaxMps=100f;
 
 #region arguments
 
 bool processArguments(string sArgument)
-{
-	sArgResults = "";
-	// string output="";
-	if (sArgument == "" || sArgument == "timer" || sArgument == "wccs" || sArgument == "wcct")
-	{
-		Echo("Arg=" + sArgument);
-		Echo("PassedArg=" + sPassedArgument);
-		if (sPassedArgument != "" && sPassedArgument != "timer")
-		{
-			Echo("Using Passed Arg=" + sPassedArgument);
-			sArgument = sPassedArgument;
-		}
-	}
+        {
+            sArgResults = "";
+            // string output="";
+            if (sArgument == "" || sArgument == "timer" || sArgument == "wccs" || sArgument == "wcct")
+            {
+                Echo("Arg=" + sArgument);
+                Echo("PassedArg=" + sPassedArgument);
+                if (sPassedArgument != "" && sPassedArgument != "timer")
+                {
+                    Echo("Using Passed Arg=" + sPassedArgument);
+                    sArgument = sPassedArgument;
+                }
+            }
 
-	if(sArgument=="init")
-	{
-		sInitResults = "";
-		init = false;
-		currentInit = 0;
-		doInit(); // do first pass.
-		return false;
-	}
+            if (sArgument == "init")
+            {
+                sInitResults = "";
+                init = false;
+                currentInit = 0;
+                doInit(); // do first pass.
+                return false;
+            }
 
-	string[] args = sArgument.Trim().Split(' ');
+            string[] args = sArgument.Trim().Split(' ');
 
-	if (args[0] == "timer")
-	{
-		// do nothing for sub-module
-	}
-	else
-	if (args[0] == "setmaxspeed")
-	{
-		if (args.Length < 2)
-		{
-			Echo("Invalid arg");
-			return false;
-		}
-		float fValue = 0;
-		bool fOK = float.TryParse(args[1].Trim(), out fValue);
-		if (!fOK)
-		{
-			Echo("invalid float value:" + args[1]);
-			return false;
-		}
-		fMaxMps = fValue;
-		sArgResults = "max speed set to " + fMaxMps.ToString() + "mps";
+            if (args[0] == "timer")
+            {
+                // do nothing for sub-module
+            }
+            else
+            if (args[0] == "setmaxspeed")
+            {
+                if (args.Length < 2)
+                {
+                    Echo("Invalid arg");
+                    return false;
+                }
+                float fValue = 0;
+                bool fOK = float.TryParse(args[1].Trim(), out fValue);
+                if (!fOK)
+                {
+                    Echo("invalid float value:" + args[1]);
+                    return false;
+                }
+                fMaxMps = fValue;
+                sArgResults = "max speed set to " + fMaxMps.ToString() + "mps";
 
-	}
-	else if (args[0] == "resetlaunch")
-	{
-		bValidHome = false;
-		bValidLaunch1 = false;
-	}
-	else if (args[0] == "wccs")
-	{
+            }
+            else if (args[0] == "resetlaunch")
+            {
+                bValidHome = false;
+                bValidLaunch1 = false;
+            }
+            else if (args[0] == "wccs")
+            {
 
-	}
-	else if (args[0] == "wcct")
-	{
+            }
+            else if (args[0] == "wcct")
+            {
 
-	}
-	else
-	{
-		int iDMode;
-		if (modeCommands.TryGetValue(args[0].ToLower(), out iDMode))
-		{
-			sArgResults = "mode set to " + iDMode;
-			setMode(iDMode);
-			// return true;
-		}
-		else
-		{
-			sArgResults = "Unknown argument:" + args[0];
-		}
-	}
-	return false; // keep processing in main
-}
-#endregion
-  
+            }
+            else
+            {
+                int iDMode;
+                if (modeCommands.TryGetValue(args[0].ToLower(), out iDMode))
+                {
+                    sArgResults = "mode set to " + iDMode;
+                    setMode(iDMode);
+                    // return true;
+                }
+                else
+                {
+                    sArgResults = "Unknown argument:" + args[0];
+                }
+            }
+            return false; // keep processing in main
+        }
+        #endregion
+
 
     }
 }
