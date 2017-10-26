fe5d6c51756cedd9cd6a1da248fae0ad1bb7ff71
Wicorel <saettler@hotmail.com>
2017-10-26 07:31:52 -0700

    Tab formatting

--- WicoDrills/WicoDrills/WicoDrills.cs
+++ WicoDrills/WicoDrills/WicoDrills.cs
@@ -18,33 +18,33 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-#region drills
+        #region drills
 
-List <IMyTerminalBlock> drillList = new List <IMyTerminalBlock>();
-string drillInit()
-{
-    List<IMyTerminalBlock> Output = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> drillList = new List<IMyTerminalBlock>();
+        string drillInit()
+        {
+            List<IMyTerminalBlock> Output = new List<IMyTerminalBlock>();
 
-	drillList.Clear();
-	Output = GetTargetBlocks<IMyShipDrill>();
-	foreach (var b in Output)
-		drillList.Add(b as IMyTerminalBlock);
-	return "D" + drillList.Count.ToString("00");
-}
-#endregion
-#region ejectors
+            drillList.Clear();
+            Output = GetTargetBlocks<IMyShipDrill>();
+            foreach (var b in Output)
+                drillList.Add(b as IMyTerminalBlock);
+            return "D" + drillList.Count.ToString("00");
+        }
+        #endregion
+        #region ejectors
 
-List <IMyTerminalBlock> ejectorList = new List <IMyTerminalBlock>();
-string ejectorsInit()
-{
-    List<IMyTerminalBlock> Output = new List<IMyTerminalBlock>();
-	ejectorList.Clear();
-	Output=GetBlocksContains<IMyShipConnector>("Ejector");
-	foreach (var b in Output)
-		ejectorList.Add(b as IMyTerminalBlock);
-	return "E" + ejectorList.Count.ToString("00");
-}
-#endregion
+        List<IMyTerminalBlock> ejectorList = new List<IMyTerminalBlock>();
+        string ejectorsInit()
+        {
+            List<IMyTerminalBlock> Output = new List<IMyTerminalBlock>();
+            ejectorList.Clear();
+            Output = GetBlocksContains<IMyShipConnector>("Ejector");
+            foreach (var b in Output)
+                ejectorList.Add(b as IMyTerminalBlock);
+            return "E" + ejectorList.Count.ToString("00");
+        }
+        #endregion
 
     }
 }
