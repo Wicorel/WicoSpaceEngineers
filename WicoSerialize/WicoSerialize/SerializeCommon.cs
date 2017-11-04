d3c20a8db501d6bf41dc6ae5c1ccb986debefa36
Wicorel <saettler@hotmail.com>
2017-11-04 15:04:56 -0700

    Tab formating

--- WicoSerialize/WicoSerialize/SerializeCommon.cs
+++ WicoSerialize/WicoSerialize/SerializeCommon.cs
@@ -19,139 +19,137 @@ namespace IngameScript
     partial class Program : MyGridProgram
     {
 
-        void DoNothing() { return; }
+        // V3.0 - redo all variables & cleanup
+        #region serializecommon
+        const string SAVE_FILE_NAME = "Wico Craft Save";
+        float savefileversion = 3.00f;
+        IMyTextPanel SaveFile = null;
 
-// V3.0 - redo all variables & cleanup
-#region serializecommon
-const string SAVE_FILE_NAME = "Wico Craft Save";
-float savefileversion = 3.00f;
-IMyTextPanel SaveFile = null;
+        // Saved info:
+        int current_state = 0;
 
-// Saved info:
-int current_state = 0;
+        long allBlocksCount = 0;
 
-long allBlocksCount = 0;
+        Vector3D vCurrentPos;
+        Vector3D vDock;
+        Vector3D vLaunch1;
+        Vector3D vHome;
+        bool bValidDock = false;
+        bool bValidLaunch1 = false;
+        bool bValidHome = false;
+        double dGravity = -2;
+        int craft_operation = CRAFT_MODE_AUTO;
+        int currentRun = 0;
+        string sPassedArgument = "";
 
-Vector3D vCurrentPos;
-Vector3D vDock;
-Vector3D vLaunch1;
-Vector3D vHome;
-bool bValidDock = false;
-bool bValidLaunch1 = false;
-bool bValidHome = false;
-double dGravity = -2;
-int craft_operation = CRAFT_MODE_AUTO;
-int currentRun = 0;
-string sPassedArgument = "";
+        // valid vectors
+        bool bValidInitialContact = false;
+        bool bValidInitialExit = false;
+        bool bValidTarget = false;
+        bool bValidAsteroid = false;
+        bool bValidNextTarget = false;
 
-// valid vectors
-bool bValidInitialContact = false;
-bool bValidInitialExit = false;
-bool bValidTarget = false;
-bool bValidAsteroid = false;
-bool bValidNextTarget = false;
+        // operation flags
+        bool bAutopilotSet = true;
+        bool bAutoRelaunch = false;
 
-// operation flags
-bool bAutopilotSet = true;
-bool bAutoRelaunch = false;
+        // 
+        int iAlertStates = 0;
 
-// 
-int iAlertStates = 0;
+        // time outs
+        DateTime dtStartShip;
+        DateTime dtStartCargo;
+        DateTime dtStartSearch;
+        DateTime dtStartMining;
+        DateTime dtLastRan;
+        DateTime dtStartNav;
 
-// time outs
-DateTime dtStartShip;
-DateTime dtStartCargo;
-DateTime dtStartSearch;
-DateTime dtStartMining;
-DateTime dtLastRan;
-DateTime dtStartNav;
+        // positions
+        //Vector3D vLastPos;
+        Vector3D vInitialContact;
+        Vector3D vInitialExit;
+        Vector3D vLastContact;
+        Vector3D vLastExit;
+        Vector3D vTargetMine;
+        Vector3D vTargetAsteroid;
+        Vector3D vCurrentNavTarget;
+        Vector3D vNextTarget;
+        Vector3D vExpectedExit;
 
-// positions
-//Vector3D vLastPos;
-Vector3D vInitialContact;
-Vector3D vInitialExit;
-Vector3D vLastContact;
-Vector3D vLastExit;
-Vector3D vTargetMine;
-Vector3D vTargetAsteroid;
-Vector3D vCurrentNavTarget;
-Vector3D vNextTarget;
-Vector3D vExpectedExit;
+        // detection
+        int iDetects = 0;
+        int batterypcthigh = 80;
+        int batterypctlow = 20;
+        int batteryPercentage = -1;
 
-// detection
-int iDetects = 0;
-int batterypcthigh = 80;
-int batterypctlow = 20;
-int batteryPercentage = -1;
+        int cargopctmin = 5;
+        int cargopcent = -1;
+        double cargoMult = -1;
 
-int cargopctmin = 5;
-int cargopcent = -1;
-double cargoMult = -1;
+        // tanks
+        double hydroPercent = -1;
+        double oxyPercent = -1;
 
-// tanks
-double hydroPercent = -1;
-double oxyPercent = -1;
+        double totalMaxPowerOutput = 0;
+        double maxReactorPower = -1;
+        double maxSolarPower = -1;
+        double maxBatteryPower = -1;
 
-double totalMaxPowerOutput = 0;
-double maxReactorPower = -1;
-double maxSolarPower = -1;
-double maxBatteryPower = -1;
+        string sReceivedMessage = "";
 
-string sReceivedMessage = "";
+        string initSerializeCommon()
+        {
 
-string initSerializeCommon()
-{
+            string sInitResults = "S";
 
-    string sInitResults = "S";
+            SaveFile = null;
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+            blocks = GetBlocksNamed<IMyTextPanel>(SAVE_FILE_NAME);
 
-	SaveFile = null;
-    List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
-    blocks = GetBlocksNamed<IMyTextPanel>(SAVE_FILE_NAME);
+            if (blocks.Count > 1) Echo("Multiple blocks found: \"" + SAVE_FILE_NAME + "\"");
+            else if (blocks.Count == 0) Echo("Missing: " + SAVE_FILE_NAME);
+            else SaveFile = blocks[0] as IMyTextPanel;
 
-	if (blocks.Count > 1) Echo("Multiple blocks found: \"" + SAVE_FILE_NAME + "\"");
-	else if (blocks.Count == 0) Echo("Missing: " + SAVE_FILE_NAME);
-	else SaveFile = blocks[0] as IMyTextPanel;
+            if (SaveFile == null)
+            {
+                sInitResults = "-";
+                Echo(SAVE_FILE_NAME + " (TextPanel) is missing or Named incorrectly. ");
+            }
+            return sInitResults;
+        }
 
-    if (SaveFile == null)
-    {
-        sInitResults = "-";
-        Echo(SAVE_FILE_NAME + " (TextPanel) is missing or Named incorrectly. ");
-    }
-    return sInitResults;
-}
+        bool validSavefile()
+        {
+            return SaveFile != null;
+        }
+        string Vector3DToString(Vector3D v)
+        {
+            string s;
+            s = v.X.ToString("0.00") + ":" + v.Y.ToString("0.00") + ":" + v.Z.ToString("0.00");
+            //    s = v.GetDim(0) + ":" + v.GetDim(1) + ":" + v.GetDim(2);
+            return s;
+        }
+        bool ParseVector3d(string sVector, out double x, out double y, out double z)
+        {
+            string[] coordinates = sVector.Trim().Split(',');
+            if (coordinates.Length < 3)
+            {
+                coordinates = sVector.Trim().Split(':');
+            }
+            bool xOk = double.TryParse(coordinates[0].Trim(), out x);
+            bool yOk = double.TryParse(coordinates[1].Trim(), out y);
+            bool zOk = double.TryParse(coordinates[2].Trim(), out z);
+            if (!xOk || !yOk || !zOk)
+            {
+                return false;
+            }
+            return true;
+        }
 
-bool validSavefile()
-{
-	return SaveFile != null;
-}
-string Vector3DToString(Vector3D v)
-{
-    string s;
-    s = v.X.ToString("0.00") + ":" + v.Y.ToString("0.00") + ":" + v.Z.ToString("0.00");
-//    s = v.GetDim(0) + ":" + v.GetDim(1) + ":" + v.GetDim(2);
-    return s;
-}
-bool ParseVector3d(string sVector, out double x, out double y, out double z)
-{
-    string[] coordinates = sVector.Trim().Split(',');
-    if (coordinates.Length < 3)
-    {
-        coordinates = sVector.Trim().Split(':');
-    }
-    bool xOk = double.TryParse(coordinates[0].Trim(), out x);
-    bool yOk = double.TryParse(coordinates[1].Trim(), out y);
-    bool zOk = double.TryParse(coordinates[2].Trim(), out z);
-    if (!xOk || !yOk || !zOk)
-    {
-        return false;
-    }
-    return true;
-}
+        #endregion
 
-#endregion
-
-// state variables
-string sLastLoad = "";
+        // state variables
+        string sLastLoad = "";
 
     }
 }
