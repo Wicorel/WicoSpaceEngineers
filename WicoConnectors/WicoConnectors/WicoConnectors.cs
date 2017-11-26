432ab65b85d921f9d68737f01b303840eafe2f44
Wicorel <saettler@hotmail.com>
2017-11-26 10:17:12 -0800

    formatting and ???

--- WicoConnectors/WicoConnectors/WicoConnectors.cs
+++ WicoConnectors/WicoConnectors/WicoConnectors.cs
@@ -19,147 +19,157 @@ namespace IngameScript
     partial class Program : MyGridProgram
     {
 
-//03/30 Fix connectany to use localdock
-//03/27 cache optimizations
-// starts looking for "[DOCK]"
-// also looks for [BASE] and excludes those
-// 01/07/2017
-// 01/10 added other connector
-// 01/24 1.172 PB API changes
-#region connectors
-List<IMyTerminalBlock> localConnectors = new List<IMyTerminalBlock>();
-List<IMyTerminalBlock> localDockConnectors = new List<IMyTerminalBlock>();
-List<IMyTerminalBlock> localBaseConnectors = new List<IMyTerminalBlock>();
+        //03/30 Fix connectany to use localdock
+        //03/27 cache optimizations
+        // starts looking for "[DOCK]"
+        // also looks for [BASE] and excludes those
+        // 01/07/2017
+        // 01/10 added other connector
+        // 01/24 1.172 PB API changes
+        #region connectors
+        List<IMyTerminalBlock> localConnectors = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> localDockConnectors = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> localBaseConnectors = new List<IMyTerminalBlock>();
 
-bool bConnectorsInit = false;
+        bool bConnectorsInit = false;
 
-string connectorsInit()
-{
-	bConnectorsInit = false;
-	localConnectors.Clear();
-	localDockConnectors.Clear();
-	localBaseConnectors.Clear();
-	getLocalConnectors();
-	return "CL" + localConnectors.Count.ToString()+ "CD"+localDockConnectors.Count.ToString()+ "CB"+localBaseConnectors.Count.ToString();
-}
+        string connectorsInit()
+        {
+            bConnectorsInit = false;
+            localConnectors.Clear();
+            localDockConnectors.Clear();
+            localBaseConnectors.Clear();
+            getLocalConnectors();
+            return "CL" + localConnectors.Count.ToString() + "CD" + localDockConnectors.Count.ToString() + "CB" + localBaseConnectors.Count.ToString();
+        }
 
-void getLocalConnectors()
-{
-	if (localConnectors.Count < 1 && !bConnectorsInit) localConnectors = GetTargetBlocks<IMyShipConnector>();
+        void getLocalConnectors()
+        {
+            if (localConnectors.Count < 1 && !bConnectorsInit) localConnectors = GetTargetBlocks<IMyShipConnector>();
 
-	if (localDockConnectors.Count < 1 && !bConnectorsInit) localDockConnectors = GetBlocksContains<IMyShipConnector>("[DOCK]");
-	if (localDockConnectors.Count < 1 && !bConnectorsInit) localDockConnectors = localConnectors;
-	if (localBaseConnectors.Count < 1 && !bConnectorsInit) localBaseConnectors = GetBlocksContains<IMyShipConnector>("[BASE]");
-	bConnectorsInit = true;
-	return;
-}
-bool AnyConnectorIsLocked()
-{
-	getLocalConnectors();
-
-	for (int i = 0; i < localDockConnectors.Count; i++)
-	{
-		IMyShipConnector sc = localDockConnectors[i] as IMyShipConnector;
-		if (sc.Status == MyShipConnectorStatus.Connectable)
-//		if (sc.IsLocked)
-			return true;
-	}
-	return false;
-}
-
-bool AnyConnectorIsConnected()
-{
-	getLocalConnectors();
-
-	for (int i = 0; i < localDockConnectors.Count; i++)
-	{
-		IMyShipConnector sc = localDockConnectors[i] as IMyShipConnector;
-		if (sc.Status == MyShipConnectorStatus.Connected)
-		{
-			IMyShipConnector sco = sc.OtherConnector;
-			if (sco.CubeGrid == sc.CubeGrid)
-			{
-				//Echo("Locked-but connected to 'us'");
-				continue;
-			}
-			else return true;
-		}
-	}
-	return false;
-}
-
-IMyTerminalBlock getDockingConnector() // maybe pass in prefered orientation?
-{ // dumb mode for now.
+            if (localDockConnectors.Count < 1 && !bConnectorsInit) localDockConnectors = GetBlocksContains<IMyShipConnector>("[DOCK]");
+            if (localDockConnectors.Count < 1 && !bConnectorsInit) localDockConnectors = localConnectors;
+            if (localBaseConnectors.Count < 1 && !bConnectorsInit) localBaseConnectors = GetBlocksContains<IMyShipConnector>("[BASE]");
+            bConnectorsInit = true;
+            return;
+        }
+        bool AnyConnectorIsLocked()
+        {
             getLocalConnectors();
 
-	if(localDockConnectors.Count>0)
-	{
-//	Echo("Found local Connector");
-		return localDockConnectors[0];
-	}
-//Echo("NO local connectors");
-	return null;
-}
+            for (int i = 0; i < localDockConnectors.Count; i++)
+            {
+                IMyShipConnector sc = localDockConnectors[i] as IMyShipConnector;
+                if (sc.Status == MyShipConnectorStatus.Connectable)
+                    //		if (sc.IsLocked)
+                    return true;
+            }
+            return false;
+        }
 
-IMyTerminalBlock getConnectedConnector()
-{
-	getLocalConnectors();
+        bool AnyConnectorIsConnected()
+        {
+            getLocalConnectors();
 
-	for (int i = 0; i < localDockConnectors.Count; i++)
-	{
-		IMyShipConnector sc = localDockConnectors[i] as IMyShipConnector;
-		if (sc.Status == MyShipConnectorStatus.Connected)
-		{
-			IMyShipConnector sco = sc.OtherConnector;
-			if (sco.CubeGrid == sc.CubeGrid)
-			{
-				continue;
-			}
-			else return sc.OtherConnector;
-		}
-	}
-	return null;
-}
+            for (int i = 0; i < localDockConnectors.Count; i++)
+            {
+                IMyShipConnector sc = localDockConnectors[i] as IMyShipConnector;
+                if (sc.Status == MyShipConnectorStatus.Connected)
+                {
+                    IMyShipConnector sco = sc.OtherConnector;
+                    if (sco.CubeGrid == sc.CubeGrid)
+                    {
+                        //Echo("Locked-but connected to 'us'");
+                        continue;
+                    }
+                    else return true;
+                }
+            }
+            return false;
+        }
 
-void ConnectAnyConnectors(bool bConnect = true, string sAction = "")
-{
-	getLocalConnectors();
-//	Echo("CCA:"+ localDockConnectors.Count);
-	for (int i = 0; i < localDockConnectors.Count; i++)
-	{
-		IMyShipConnector sc = localDockConnectors[i] as IMyShipConnector;
-		if (sc.Status == MyShipConnectorStatus.Connected)
-		{
-			IMyShipConnector sco = sc.OtherConnector;
-			if (sco.CubeGrid == sc.CubeGrid)
-			{
-				//Echo("Locked-but connected to 'us'");
-				continue; // skip it.
-			}
-		}
-		if (bConnect)
-		{
-			if (sc.Status == MyShipConnectorStatus.Connectable) sc.ApplyAction("SwitchLock");
-		}
-		else
-		{
-			if (sc.Status == MyShipConnectorStatus.Connected) sc.ApplyAction("SwitchLock");
-		}
+        IMyTerminalBlock getDockingConnector() // maybe pass in prefered orientation?
+        { // dumb mode for now.
+            getLocalConnectors();
 
-		if (sAction != "")
-		{
-			ITerminalAction ita;
-			ita = sc.GetActionWithName(sAction);
-			if (ita != null) ita.Apply(sc);
-		}
+            if (localDockConnectors.Count > 0)
+            {
+                //	Echo("Found local Connector");
+                return localDockConnectors[0];
+            }
+            //Echo("NO local connectors");
+            return null;
+        }
 
-	}
-	return;
-}
+        IMyTerminalBlock getConnectedConnector(bool bMe = false)
+        {
 
-#endregion
+            getLocalConnectors();
+
+            for (int i = 0; i < localDockConnectors.Count; i++)
+            {
+                IMyShipConnector sc = localDockConnectors[i] as IMyShipConnector;
+                if (sc.Status == MyShipConnectorStatus.Connected)
+                {
+                    IMyShipConnector sco = sc.OtherConnector;
+                    if (sco.CubeGrid == sc.CubeGrid)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (!bMe)
+                        {
+                            return sc.OtherConnector;
+                        }
+                        else
+                        {
+                            return localDockConnectors[i];
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        void ConnectAnyConnectors(bool bConnect = true, string sAction = "")
+            {
+                getLocalConnectors();
+                //	Echo("CCA:"+ localDockConnectors.Count);
+                for (int i = 0; i < localDockConnectors.Count; i++)
+                {
+                    IMyShipConnector sc = localDockConnectors[i] as IMyShipConnector;
+                    if (sc.Status == MyShipConnectorStatus.Connected)
+                    {
+                        IMyShipConnector sco = sc.OtherConnector;
+                        if (sco.CubeGrid == sc.CubeGrid)
+                        {
+                            //Echo("Locked-but connected to 'us'");
+                            continue; // skip it.
+                        }
+                    }
+                    if (bConnect)
+                    {
+                        if (sc.Status == MyShipConnectorStatus.Connectable) sc.ApplyAction("SwitchLock");
+                    }
+                    else
+                    {
+                        if (sc.Status == MyShipConnectorStatus.Connected) sc.ApplyAction("SwitchLock");
+                    }
+
+                    if (sAction != "")
+                    {
+                        ITerminalAction ita;
+                        ita = sc.GetActionWithName(sAction);
+                        if (ita != null) ita.Apply(sc);
+                    }
+
+                }
+                return;
+            }
+
+            #endregion
 
 
 
+        }
     }
-}
