55aa74dcc7c31f48676fc772e1e9f235b7de1e6b
Wicorel <saettler@hotmail.com>
2018-01-06 12:20:38 -0800

    Add battery checks when docked (needs to check NOPOWER flag)

--- ModeDocked/ModeDocked/ModeDocked.cs
+++ ModeDocked/ModeDocked/ModeDocked.cs
@@ -19,6 +19,16 @@ namespace IngameScript
     partial class Program : MyGridProgram
     {
 
+        /*
+         * TODO:
+         * 
+         * Unload ship when docked..
+         * 
+         * Load uranium
+         * stockpile tanks
+         * battery charge
+         */
+
         #region docked
         // 0 = master init
         // 1 = inited.
@@ -27,7 +37,7 @@ namespace IngameScript
         {
             StatusLog("clear", textPanelReport);
             StatusLog(moduleName + ":DOCKED!", textPanelReport);
-
+            Echo("Docked!");
             if (bAutoRelaunch && bValidDock)
             {
                 Echo("Docked. Checking Relaunch");
@@ -53,19 +63,68 @@ namespace IngameScript
             {
 
                 StatusLog(moduleName + ":Power Saving Mode", textPanelReport);
+                Echo("Power Saving Mode");
                 if (current_state == 0)
                 {
+                    if ((craft_operation & CRAFT_MODE_NOTANK) == 0) TanksStockpile(true);// blockApplyAction(tankList, "Stockpile_On");
                     powerDownThrusters(thrustAllList, thrustAll, true);
                     antennaLowPower();
                     sleepAllSensors();
-                    // turn gyos off?
+                    // TODO: ??? turn gyos off?
+                    batteryCheck(0, true);
                     current_state = 1;
                 }
- //               Vector3D vVec = calcBlockForwardVector(gpsCenter);
-//                IMyTerminalBlock otherConnector = getConnectedConnector();
+                else if (current_state == 1)
+                {
+                    batteryCheck(0, true);
+                    if (batteryPercentage < 0 || (craft_operation & CRAFT_MODE_NOPOWERMGMT) == 0 )
+                        current_state = 4; // skip battery checks
+                    else
+                                                   if (!batteryCheck(30, true))
+                        current_state = 2;
+                }
+                else if (current_state == 2)
+                {
+                    if (!batteryCheck(80, true))
+                        current_state = 3;
+                }
+                else if (current_state == 3)
+                {
+                    if (!batteryCheck(100, true))
+                        current_state = 1; // go back and check again
+                }
+                else batteryCheck(0, true); //,textBlock);
+
+                ///
+                {
+                    if (batteryPercentage >= 0) StatusLog("Bat:" + progressBar(batteryPercentage), textPanelReport);
+                    else Echo("No Batteries");
+                    if (oxyPercent >= 0)
+                    {
+                        StatusLog("O2:" + progressBar(oxyPercent * 100), textPanelReport);
+                        //Echo("O:" + oxyPercent.ToString("000.0%"));
+                    }
+                    else Echo("No Oxygen Tanks");
+
+                    if (hydroPercent >= 0)
+                    {
+                        StatusLog("Hyd:" + progressBar(hydroPercent * 100), textPanelReport);
+                        if (hydroPercent < 0.20f)
+                            StatusLog(" WARNING: Low Hydrogen Supplies", textPanelReport);
+
+                        Echo("H:" + hydroPercent.ToString("000.0%"));
+                    }
+                    else Echo("No Hydrogen Tanks");
+                    if (batteryPercentage >=0 && batteryPercentage < batterypctlow)
+                        StatusLog(" WARNING: Low Battery Power", textPanelReport);
+
+                    // TODO: get uranium into reactors; take out excess ingots; turn off conveyor usage (like TIM)
+                    // TODO: get ore OUT of ship and into base (including stone)
+                    // TODO: Handle ore carrier/shuttle
+                }
             }
 
-        }
+    }
         #endregion
 
     }
