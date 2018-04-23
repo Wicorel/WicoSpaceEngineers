c4ba928252b586dff5266f97e809d7fa75e3645e
Wicorel <saettler@hotmail.com>
2018-04-22 22:30:53 -0700

    Add calculteBestGravityThrust() to dynamically determine best launch profile.

--- WicoOrbitalCommon/WicoOrbitalCommon/OrbitalCommon.cs
+++ WicoOrbitalCommon/WicoOrbitalCommon/OrbitalCommon.cs
@@ -12,10 +12,17 @@ namespace IngameScript
         Vector3D vOrbitalHome;
         bool bValidOrbitalHome = false;
 
+        float orbitalAtmoMult = 5;
+        float orbitalIonMult = 2;
+        float orbitalHydroMult = 1;
+
         string sOrbitalSection = "ORBITAL";
 
         void OrbitalInitCustomData(INIHolder iNIHolder)
         {
+            iNIHolder.SetValue(sOrbitalSection, "AtmoMult", orbitalAtmoMult);
+            iNIHolder.SetValue(sOrbitalSection, "IonMult", orbitalIonMult);
+            iNIHolder.SetValue(sOrbitalSection, "HydroMult", orbitalHydroMult);
         }
 
         void OrbitalSerialize(INIHolder iNIHolder)
@@ -35,6 +42,49 @@ namespace IngameScript
 
         }
 
+        string sOrbitalUpDirection = "";
+        List<IMyTerminalBlock> thrustOrbitalUpList = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> thrustOrbitalDownList = new List<IMyTerminalBlock>();
+
+        List<IMyTerminalBlock> cameraOrbitalLandingList = new List<IMyTerminalBlock>();
+
+        /// <summary>
+        /// Choose best thrusters and orientation to use in gravity to launch
+        /// WANT: base on current power and hydrogen (and ice) availability 
+        /// </summary>
+        /// <returns>true if the 'best' has changed. Modifies thrustOrbitalUpList, thrustObritalDownList, and sOrbitalDirection</returns>
+        ///
+        bool calculateBestGravityThrust()
+        {
+            double upThrust = calculateTotalEffectiveThrust(thrustUpList, orbitalAtmoMult,orbitalIonMult, orbitalHydroMult);
+            double fwThrust = calculateTotalEffectiveThrust(thrustForwardList, orbitalAtmoMult, orbitalIonMult, orbitalHydroMult);
+            bool bChanged = false;
+
+            if (fwThrust > upThrust)
+            {
+                if (sOrbitalUpDirection != "rocket")
+                {
+                    thrustOrbitalUpList = thrustForwardList;
+                    thrustOrbitalDownList = thrustBackwardList;
+                    sOrbitalUpDirection = "rocket";
+                    bChanged = true;
+                    cameraOrbitalLandingList = cameraBackwardList;
+                }
+            }
+            else
+            {
+                if (sOrbitalUpDirection != "down")
+                {
+                    thrustOrbitalUpList = thrustUpList;
+                    thrustOrbitalDownList = thrustDownList;
+                    sOrbitalUpDirection = "down";
+                    bChanged = true;
+                    cameraOrbitalLandingList = cameraDownList;
+                }
+            }
+            return bChanged;
+
+        }
 
     }
 }
