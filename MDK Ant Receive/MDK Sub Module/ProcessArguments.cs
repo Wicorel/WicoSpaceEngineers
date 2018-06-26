0d6106883e7504f45dd29fb2c4542fb220866578
Wicorel <saettler@hotmail.com>
2018-06-26 08:27:29 -0700

    Add 'trigger' command and antenna message

--- MDK Ant Receive/MDK Sub Module/ProcessArguments.cs
+++ MDK Ant Receive/MDK Sub Module/ProcessArguments.cs
@@ -45,8 +45,20 @@ namespace IngameScript
             }
 
             string[] args = sArgument.Trim().Split(' ');
+            if (args[0] == "trigger")
+            {
+                Echo("trigger");
+                if (args.Length > 1)
+                {
+                    string sCmd = "WICO:TRIGGER:";
+                    sCmd += args[1].Trim();
+                    antSend(sCmd);
+                }
+                else Echo("Incomplete command");
 
-            if(DockProcessMessage(sArgument))
+            }
+
+            if (DockProcessMessage(sArgument))
             {
                 Echo("Processed");
             }
@@ -78,35 +90,71 @@ namespace IngameScript
                 return true;
             if (PatrolProcessMessage(sArgument))
                 return true;
+            if (ProcessTriggerMessage(sArgument))
+                return true;
+            return false;
+        }
+
+        bool ProcessTriggerMessage(string sReceivedMessage)
+        {
+            sReceivedMessage = sReceivedMessage.Trim();
+//sStartupError += "\n" + sReceivedMessage;
+            string[] aMessage = sReceivedMessage.Trim().Split(':');
+//sStartupError += "\n" + aMessage.Length + " Parts";
+//            Echo(aMessage.Length + " Parts");
+            if (aMessage.Length > 0)
+            {
+                if (aMessage[0] != "WICO")
+                {
+//sStartupError += "\nNot WICO";
+                    Echo("not wico system message");
+                    return false;
+                }
+                if (aMessage.Length > 1)
+                {
+//sStartupError += "\nType="+aMessage[1] +" Length="+aMessage.Length;
+                    if (aMessage[1] == "TRIGGER" && aMessage.Length>2)
+                    {
+//                        sStartupError+="\nTriggering:"+aMessage[2];
+                        bool bTriggered= doSubModuleTimerTriggers(aMessage[2]);
+//sStartupError+="\nTriggered="+bTriggered.ToString();
+                        return bTriggered;
+                    }
+                    else
+                    {
+//    sStartupError += "\nNot trigger or invalid";
+                    }
+                }
+            }
             return false;
         }
 
         bool PatrolProcessMessage(string sReceivedMessage )
         {
-            sStartupError+="\nPatrolProcessMessage()";
+//            sStartupError+="\nPatrolProcessMessage()";
             sReceivedMessage = sReceivedMessage.Trim();
-            sStartupError += "\n" + sReceivedMessage;
+//            sStartupError += "\n" + sReceivedMessage;
             string[] aMessage = sReceivedMessage.Trim().Split(':');
-            sStartupError += "\n" + aMessage.Length + " Parts";
+//            sStartupError += "\n" + aMessage.Length + " Parts";
             Echo(aMessage.Length + " Parts");   
             if (aMessage.Length > 1)
             {
                 if (aMessage[0] != "WICO")
                 {
-                    sStartupError += "\nNot WICO";
+//                    sStartupError += "\nNot WICO";
                     Echo("not wico system message");
                     return false;
                 }
                 if (aMessage.Length > 2)
                 {
-                    sStartupError += "\nType="+aMessage[1];
+//                    sStartupError += "\nType="+aMessage[1];
                     if (aMessage[1] == "PATROL")
                     {
-                        sStartupError+="\nPatrol request";
+ //                       sStartupError+="\nPatrol request";
                         Echo("Patrol waypoint Request!");
                         if(!bAllowPatrol)
                         {
-                            sStartupError += "\nPatrol is turned off";
+//                            sStartupError += "\nPatrol is turned off";
                             Echo("Patrol is turned off");
                             return false;
                         }
@@ -114,7 +162,7 @@ namespace IngameScript
                         rc = shipOrientationBlock as IMyRemoteControl;
                         if (rc == null)
                         {
-                            sStartupError += "\nNo Remote Control Block";
+//                            sStartupError += "\nNo Remote Control Block";
                             return false;
                         }
 
