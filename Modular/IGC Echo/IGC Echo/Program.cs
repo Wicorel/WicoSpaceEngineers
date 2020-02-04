f648323ef07665c17b329f0f67e8b3eb4acb6850
Wicorel <saettler@hotmail.com>
2020-02-04 15:52:41 -0800

    Remove wicoIGC reference/use Move starting info to readme This is the 'Simple' Echo example.

--- Modular/IGC Echo/IGC Echo/Program.cs
+++ Modular/IGC Echo/IGC Echo/Program.cs
@@ -21,22 +21,32 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
 
-        WicoIGC iWicoIGC;
-        int runcount = 0;
+        /// <summary>
+        /// A count of how many times this script has been run
+        /// </summary>
+        int _runcount = 0;
 
+        /// <summary>
+        /// This is our unique ID for our message.  We've defined the format for the message data (it's just a string)
+        /// </summary>
+        string _broadCastTag = "MDK IGC EXAMPLE 1";
+
+        /// <summary>
+        /// The broadcast listener for the channel we are interested in.
+        /// </summary>
+        IMyBroadcastListener _myBroadcastListener;
 
         public Program()
         {
+            // let them know we are alive
             Echo("Creator");
-            iWicoIGC = new WicoIGC(this, true);
 
-            // Could add public and unicast handlers here..
+            // register a broadcast channel for our tag
+            _myBroadcastListener=IGC.RegisterBroadcastListener(_broadCastTag);
+
+            // Ask to be called back --to Main()-- when a message is received
+            _myBroadcastListener.SetMessageCallback(_broadCastTag); // the callback agrument does NOT need to be the same as the tag
         }
 
         public void Save()
@@ -45,33 +55,51 @@ namespace IngameScript
 
         public void Main(string argument, UpdateType updateSource)
         {
-            runcount++;
-            Echo(runcount.ToString()+ ":"+updateSource.ToString());
+            // Calculate and Output for informational purposes
+            _runcount++;
+            Echo(_runcount.ToString()+ ":"+updateSource.ToString());
+
             if (
                 (updateSource & (UpdateType.Trigger | UpdateType.Terminal)) > 0 // run by a terminal action
                 || (updateSource & (UpdateType.Mod)) > 0 // script run by a mod
                 || (updateSource & (UpdateType.Script)) > 0 // this pb run by another script (PB)
                 )
-            {
+            { // script was run because of an action
                 if (argument != "")
                 {
-                    iWicoIGC.AddPublicHandler(argument, SimpleEchoHandler);
-                    Echo("Added handler for:" + argument);
+                    // if we are given an argument, send it out over our broadcast channel
+                    IGC.SendBroadcastMessage(_broadCastTag, argument);
+                    Echo("Sending message:\n" + argument);
                 }
             }
-            iWicoIGC.ProcessIGCMessages();
-            if( (updateSource& UpdateType.IGC) >0) // script was run because of incoming IGC message
-            {
-                // since messages can come without a callback, we process the IGC messages anyway.
+
+            if( (updateSource& UpdateType.IGC) >0)
+            { // script was run because of incoming IGC message
+                if (_myBroadcastListener.HasPendingMessage)
+                {
+                    var myIGCMessage = _myBroadcastListener.AcceptMessage();
+                    if(myIGCMessage.Tag==_broadCastTag)
+                    { // This is our tag
+                        if(myIGCMessage.Data is string)
+                        {
+                            string str = myIGCMessage.Data.ToString();
+                            Echo("Received IGC Public Message");
+                            Echo("Tag=" + myIGCMessage.Tag);
+                            Echo("Data=" + myIGCMessage.Data.ToString());
+                            Echo("Source=" + myIGCMessage.Source.ToString("X"));
+                        }
+                        else // if(msg.Data is XXX)
+                        {
+                            // handle other data types here...
+                        }
+                    }
+                    else
+                    {
+                        // handle other tags here
+                    }
+                }
             }
         }
 
-        void SimpleEchoHandler(MyIGCMessage myIGCMessage)
-        {
-            Echo("Received IGC Public Message");
-            Echo("Tag=" + myIGCMessage.Tag);
-            Echo("Data=" + myIGCMessage.Data.ToString());
-            Echo("Source=" + myIGCMessage.Source.ToString());
-        }
     }
 }
