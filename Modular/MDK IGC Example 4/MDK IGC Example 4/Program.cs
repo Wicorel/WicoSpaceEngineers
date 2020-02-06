28b47783b22042e04c6d8d61557e43fddfc09266
Wicorel <saettler@hotmail.com>
2020-02-05 20:54:49 -0800

    Example 4 Initial Commit

---
+++ Modular/MDK IGC Example 4/MDK IGC Example 4/Program.cs
@@ -0,0 +1,143 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        WicoIGC _wicoIGC;
+
+        /// <summary>
+        /// The combined set of UpdateTypes that count as a 'trigger'
+        /// </summary>
+        UpdateType _utTriggers = UpdateType.Terminal | UpdateType.Trigger | UpdateType.Mod | UpdateType.Script;
+        /// <summary>
+        /// the combined set of UpdateTypes and count as an 'Update'
+        /// </summary>
+        UpdateType _utUpdates = UpdateType.Update1 | UpdateType.Update10 | UpdateType.Update100 | UpdateType.Once;
+
+        public Program()
+        {
+            _wicoIGC = new WicoIGC(this);
+
+            // cause ourselves to run again so we can do the init
+            Runtime.UpdateFrequency = UpdateFrequency.Once;
+        }
+
+        public void Save()
+        {
+        }
+
+        /// <summary>
+        /// Has everything been initialized?
+        /// </summary>
+        bool _areWeInited = false;
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            // Echo some information aboue 'me' and why we were run
+            Echo("Source=" + updateSource.ToString());
+            Echo("Me=" + Me.EntityId.ToString("X"));
+            Echo(Me.CubeGrid.CustomName);
+
+            if (!_areWeInited)
+            {
+                InitMessageHandlers();
+                _areWeInited = true;
+            }
+
+            // use if not setting callbacks for any of the desired channels
+            //            if (bInit) wicoIGC.ProcessIGCMessages(); 
+
+            // always check for IGC messages in case some aren't using callbacks
+            _wicoIGC.ProcessIGCMessages();
+            if ((updateSource & UpdateType.IGC) > 0)
+            {
+                // we got a callback for an IGC message.  
+                // but we already processed them.
+            }
+            else if ((updateSource & _utTriggers) > 0)
+            {
+                // if we got a 'trigger' source, send out the received argument
+                IGC.SendBroadcastMessage(_broadCastTag, argument);
+                Echo("Sending Message:\n" + argument);
+            }
+            else if ((updateSource & _utUpdates) > 0)
+            {
+                // it was an automatic update
+
+                // this script doens't have anything to do
+            }
+        }
+
+        /// <summary>
+        /// This is our unique ID for our message.  We've defined the format for the message data (it's just a string)
+        /// </summary>
+        string _broadCastTag = "MDK IGC Example 4";
+        string _unicastTag = "4Ack";
+//        string _unicastTag = "MDK IGC Example 4 Ack";
+
+        void InitMessageHandlers()
+        {
+            // creates a BROADCAST channel with the specified tag and calls the handler when messages are processed
+            _wicoIGC.AddPublicHandler(_broadCastTag, TestBroadcastHandler);
+
+            // calls the handler when UNICAST messages are processed
+            _wicoIGC.AddUnicastHandler(TestUnicastHandler);
+        }
+
+        // Handler for the test broadcast messages.
+        void TestBroadcastHandler(MyIGCMessage msg)
+        {
+            // NOTE: called on ALL received messages; not just 'our' tag
+
+            if (msg.Tag != _broadCastTag)
+                return; // not our message
+
+            if (msg.Data is string)
+            {
+                Echo("Received Test Message");
+                Echo(" Source=" + msg.Source.ToString("X"));
+                Echo(" Data=\"" + msg.Data + "\"");
+                Echo(" Tag=" + msg.Tag);
+
+                // Now reply to the sender and let them know we received the message
+                // NOTE: We are re-using the broadcast
+                IGC.SendUnicastMessage(msg.Source, _unicastTag, "Acknowledge by:"+Me.CustomName);
+                Echo(" Reply Sent");
+            }
+        }
+
+        void TestUnicastHandler(MyIGCMessage msg)
+        {
+            // NOTE: Called for ALL received unicast messages
+            if (msg.Tag != _unicastTag)
+                return; // not our message
+
+            if (msg.Data is string)
+            {
+                Echo("Received Acknowledge Message");
+                Echo(" Source=" + msg.Source.ToString("X"));
+                Echo(" Data=\"" + msg.Data + "\"");
+                Echo(" Tag=" + msg.Tag);
+            }
+
+        }
+    }
+}
