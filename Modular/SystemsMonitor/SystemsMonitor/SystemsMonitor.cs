90852e74bb4fb3998f0dead340bc7a3c5fc6cc3d
Wicorel <saettler@hotmail.com>
2020-12-25 12:19:08 -0800

    Add systemsMonitor module Collects many systems into one interface to reduce interfaces needed by other modules.

---
+++ Modular/SystemsMonitor/SystemsMonitor/SystemsMonitor.cs
@@ -0,0 +1,377 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class SystemsMonitor
+        {
+            private Program _program;
+            //            private WicoControl _wicoControl;
+            //            private WicoBlockMaster _wicoBlockMaster;
+            private Connectors _connectors;
+            private WicoThrusters _thrusters;
+            private Antennas _antennas;
+            private GasTanks _tanks;
+            private WicoGyros _gyros;
+            private PowerProduction _power;
+            //            private Timers _timers;
+            //            private WicoIGC _wicoIGC;
+            //            private WicoBases _wicoBases;
+            //            private NavCommon _navCommon;
+            private CargoCheck _cargoCheck;
+            //            private Displays _displays;
+
+            public bool bAutoRefuel = true;
+            const string AutoRefuel = "AutoRefuel";
+
+            const string SystemsSection = "SYSTEMS";
+
+            public SystemsMonitor(Program program
+                //                , WicoControl wc, WicoBlockMaster wbm
+                , WicoThrusters thrusters
+                , Connectors connectors
+                , Antennas ant
+                , GasTanks gasTanks
+                , WicoGyros wicoGyros
+                , PowerProduction pp
+                //                , Timers tim
+                //                , WicoIGC iGC
+                //                , WicoBases wicoBases
+                //                , NavCommon navCommon
+                , CargoCheck cargoCheck
+                //                , Displays displays
+                )
+            {
+                _program = program;
+//                _wicoControl = wc;
+//                _wicoBlockMaster = wbm;
+                _thrusters = thrusters;
+                _connectors = connectors;
+                _antennas = ant;
+                _tanks = gasTanks;
+                _gyros = wicoGyros;
+                _power = pp;
+                //                _timers = tim;
+                //                _wicoIGC = iGC;
+                //                _wicoBases = wicoBases;
+                //                _navCommon = navCommon;
+                _cargoCheck = cargoCheck;
+                //               _displays = displays;
+
+                bAutoRefuel = _program._CustomDataIni.Get(SystemsSection, AutoRefuel).ToBoolean(bAutoRefuel);
+                _program._CustomDataIni.Set(SystemsSection, AutoRefuel, bAutoRefuel);
+
+                _program.AddTriggerHandler(ProcessTrigger);
+
+            }
+
+            public bool BatteryGo = true;
+            public bool TanksGo = true;
+            public bool ReactorsGo = true;
+            public bool CargoGo = true;
+
+
+            double _airworthyChecksElapsedMs = -1;
+            public bool DockAirWorthy(bool bForceCheck = false, bool bLaunchCheck = true, int cargohighwater = 1)
+            {
+                BatteryGo = true;
+                TanksGo = true;
+                ReactorsGo = true;
+                CargoGo = true;
+
+                if (_airworthyChecksElapsedMs >= 0)
+                    _airworthyChecksElapsedMs += _program.Runtime.TimeSinceLastRun.TotalMilliseconds;
+                bool bDoChecks = bForceCheck;
+                if (_airworthyChecksElapsedMs < 0 || _airworthyChecksElapsedMs > 0.5 * 1000)
+                {
+                    _airworthyChecksElapsedMs = 0;
+                    bDoChecks = true;
+                }
+
+                // Check battery charge
+                if (bDoChecks) _power.BatteryCheck(0, false);
+                if (bLaunchCheck)
+                {
+                    if (_power.batteryPercentage >= 0 && _power.batteryPercentage < _power.batterypcthigh)
+                    {
+                        //                        _program.ErrorLog("Battery not airworthy (launch)");
+                        BatteryGo = false;
+                    }
+
+                }
+                else
+                {
+                    // check if we need to go back and refill
+                    if (_power.batteryPercentage >= 0 && _power.batteryPercentage < _power.batterypctlow)
+                    {
+                        //                        _program.ErrorLog("Battery not airworthy");
+                        BatteryGo = false;
+                    }
+                }
+
+                // check cargo emptied
+                if (bDoChecks) _cargoCheck.doCargoCheck();
+                if (bLaunchCheck)
+                {
+                    if (_cargoCheck.cargopcent > _cargoCheck.cargopctmin)
+                    {
+                        //                        _program.ErrorLog("Cargo not airworthy (launch)");
+                        CargoGo = false;
+                    }
+                }
+                else
+                {
+                    if (_cargoCheck.cargopcent > cargohighwater)
+                    {
+                        //                        _program.ErrorLog("Cargo not airworthy");
+                        CargoGo = false;
+                    }
+                }
+                // TODO: Check H2 tanks
+                if (bDoChecks) _tanks.TanksCalculate();
+                if (bLaunchCheck)
+                {
+                    if (_tanks.HasHydroTanks() && _tanks.hydroPercent * 100 < _tanks.tankspcthigh)
+                    {
+                        //                        _program.ErrorLog("Tanks not airworthy (launch) "+_tanks.hydroPercent.ToString("0.00"));
+                        TanksGo = false;
+                    }
+                }
+                else
+                {
+                    if (_tanks.HasHydroTanks() && _tanks.hydroPercent * 100 < _tanks.tankspctlow)
+                    {
+                        //                        _program.ErrorLog("Tanks not airworthy " + _tanks.hydroPercent.ToString("0.00"));
+                        TanksGo = false;
+                    }
+                }
+                // TODO: check reactor fuel
+
+                if (BatteryGo && TanksGo && ReactorsGo && CargoGo)
+                {
+                    return true;
+                }
+                else return false;
+
+            }
+            /// <summary>
+            /// Handler for processing any of the 'trigger' upatetypes
+            /// </summary>
+            /// <param name="argument"></param>
+            /// <param name="updateSource"></param>
+            public void ProcessTrigger(string sArgument, MyCommandLine myCommandLine, UpdateType updateSource)
+            {
+                string[] varArgs = sArgument.Trim().Split(';');
+
+                for (int iArg = 0; iArg < varArgs.Length; iArg++)
+                {
+                    string[] args = varArgs[iArg].Trim().Split(' ');
+                    // Commands here:
+
+                }
+                if (myCommandLine != null)
+                {
+                    if (myCommandLine.Argument(0) == "refuel")
+                    {
+                        bAutoRefuel = !bAutoRefuel;
+                        string s = "Autorefuel=" + bAutoRefuel.ToString();
+                        _program.Echo(s);
+                        _program.ErrorLog(s);
+                        _program._CustomDataIni.Set(SystemsSection, AutoRefuel, bAutoRefuel);
+                        _program.CustomDataChanged();
+                    }
+                    for (int arg = 0; arg < myCommandLine.ArgumentCount; arg++)
+                    {
+                        string sArg = myCommandLine.Argument(arg);
+                        // commands here:
+                    }
+                }
+            }
+
+            public bool HasHydroTanks()
+            {
+                return _tanks.HasHydroTanks();
+            }
+
+            public double oxyPercent { get { return _tanks.oxyPercent; } }
+
+            public double hydroPercent { get { return _tanks.hydroPercent; } }
+
+            public int tankspcthigh { get { return _tanks.tankspcthigh; } }
+
+            public void TanksCalculate()
+            {
+                _tanks.TanksCalculate();
+            }
+
+            public void TanksStockPile(bool bStockPile = true, int iTypes = 255)
+            {
+                _tanks.TanksStockpile(bStockPile, iTypes);
+            }
+
+            public void BatterySetNormal()
+            {
+                _power.BatterySetNormal();
+            }
+            public bool BatteryCheck(int targetMax, bool bEcho = true, bool bProgress = false)
+            {
+                return _power.BatteryCheck(targetMax, bEcho, bProgress);
+            }
+            public bool HasBatteries()
+            {
+                return _power.HasBatteries();
+            }
+            public int batteryPercentage { get { return _power.batteryPercentage; } }
+            public int batterypctlow { get { return _power.batterypctlow; } }
+            public int batterypcthigh { get { return _power.batterypcthigh; } }
+
+            public int cargopcent { get { return _cargoCheck.cargopcent; } }
+
+            public int cargopctmin { get { return _cargoCheck.cargopctmin; } }
+            public int cargohighwater { get { return _cargoCheck.cargohighwater; } }
+
+            public void doCargoCheck()
+            {
+                _cargoCheck.doCargoCheck();
+            }
+
+            public void RequestRefuel()
+            {
+                if (bAutoRefuel)
+                {
+                    _tanks.TanksStockpile(true);
+                    _power.BatteryCheck(0, true);
+                }
+            }
+
+            public void RequestLaunchSettings()
+            {
+                _tanks.TanksStockpile(false);
+                _power.BatterySetNormal();
+                _connectors.TurnEjectorsOff();
+                _thrusters.powerDownThrusters(); // turns ON all thrusters.
+            }
+
+            public bool AnyConnectorIsConnected()
+            {
+                return _connectors.AnyConnectorIsConnected();
+            }
+            public bool AnyConnectorIsLocked()
+            {
+                return _connectors.AnyConnectorIsLocked();
+            }
+            public IMyTerminalBlock GetConnectedConnector(bool bMe = false)
+            {
+                return _connectors.GetConnectedConnector(bMe);
+            }
+            public void ConnectAnyConnectors(bool bConnect = true, bool bOn = true)
+            {
+                _connectors.ConnectAnyConnectors(bConnect, bOn);
+            }
+            public IMyTerminalBlock GetDockingConnector() // maybe pass in prefered orientation?
+            {
+                return _connectors.GetDockingConnector();
+            }
+
+            public void TurnEjectorsOff()
+            {
+                _connectors.TurnEjectorsOff();
+            }
+            public void TurnEjectorsOn()
+            {
+                _connectors.TurnEjectorsOn();
+            }
+
+            public double currentTotalOutput { get { return _power.currentTotalOutput; } }
+            public double maxTotalPower { get { return _power.maxTotalPower; } }
+
+            public int EnginesCount()
+            {
+                return _power.EnginesCount();
+            }
+            public bool EnginesAreOff()
+            {
+                return _power.EnginesAreOff();
+            }
+
+            public void gyrosOff()
+            {
+                _gyros.gyrosOff();
+            }
+            public bool AlignGyros(string argument, Vector3D vDirection, IMyTerminalBlock gyroControlPoint)
+            {
+                return _gyros.AlignGyros(argument, vDirection, gyroControlPoint);
+            }
+            public void SetMinAngle(float angleRad = 0.01f)
+            {
+                _gyros.SetMinAngle(angleRad);
+            }
+            public bool BeamRider(Vector3D vStart, Vector3D vEnd, IMyTerminalBlock OrientationBlock)
+            {
+                return _gyros.BeamRider(vStart, vEnd, OrientationBlock);
+            }
+
+
+            public void ResetMotion()
+            {
+                _gyros.gyrosOff();
+                _thrusters.powerDownThrusters();
+
+            }
+
+
+            public void ThrustersCalculateOrientation(IMyTerminalBlock orientationBlock, ref List<IMyTerminalBlock> thrustForwardList,
+                ref List<IMyTerminalBlock> thrustBackwardList, ref List<IMyTerminalBlock> thrustDownList, ref List<IMyTerminalBlock> thrustUpList,
+                ref List<IMyTerminalBlock> thrustLeftList, ref List<IMyTerminalBlock> thrustRightList)
+            {
+                _thrusters.ThrustersCalculateOrientation(orientationBlock
+                    , ref thrustForwardList, ref thrustBackwardList
+                    , ref thrustDownList, ref thrustUpList
+                    , ref thrustLeftList, ref thrustRightList);
+            }
+            public int powerUpThrusters(List<IMyTerminalBlock> thrusters, float fPower = 100f, int iTypes = WicoThrusters.thrustAll)
+            {
+                return _thrusters.powerUpThrusters(thrusters, fPower, iTypes);
+
+            }
+            public int powerDownThrusters(int iTypes = WicoThrusters.thrustAll, bool bForceOff = false)
+            {
+                return _thrusters.powerDownThrusters(iTypes, bForceOff);
+            }
+            public int powerDownThrusters(List<IMyTerminalBlock> thrusters, int iTypes = WicoThrusters.thrustAll, bool bForceOff = false)
+            {
+                return _thrusters.powerDownThrusters(thrusters, iTypes, bForceOff);
+            }
+            public double calculateStoppingDistance(double physicalMass, List<IMyTerminalBlock> thrustStopList, double currentV, double dGrav)
+            {
+                return _thrusters.calculateStoppingDistance(physicalMass, thrustStopList, currentV, dGrav);
+            }
+            public void MoveForwardSlowReset()
+            {
+                _thrusters.MoveForwardSlowReset();
+            }
+            public void MoveForwardSlow(float fTarget, float fAbort, List<IMyTerminalBlock> mfsForwardThrust,
+                List<IMyTerminalBlock> mfsBackwardThrust, double effectiveMass, double shipSpeed)
+            {
+                _thrusters.MoveForwardSlow(fTarget, fAbort, mfsForwardThrust, mfsBackwardThrust, effectiveMass, shipSpeed);
+            }
+        }
+    }
+}
+        
